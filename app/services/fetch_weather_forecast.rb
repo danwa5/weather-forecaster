99195febe76e035370438d3b4ad76d2400cdd61f
Daniel Lee <danwa5@gmail.com>
2023-06-22 09:09:42 -0700

    Cache forecast data by zip code for 30 minutes

--- app/services/fetch_weather_forecast.rb
+++ app/services/fetch_weather_forecast.rb
@@ -1,6 +1,7 @@
 require 'dry/monads'
 
 class FetchWeatherForecast
+  include Dry::Monads[:result, :do]
   include Dry::Monads[:try]
   attr_reader :zip_code
 
@@ -10,11 +11,35 @@ class FetchWeatherForecast
 
   def call
     Try do
-      api = WeatherApiService.new
+      # check if cached data exists for zip code
+      cached_data = fetch_cached_results(zip_code)
+      return Success(cached_data) unless cached_data.nil?
+
+      # make api call to Weather Api
       res = api.http_get({ 'q' => zip_code })
       raise Errors::WeatherApiServiceError, "Error fetching weather forecast for #{zip_code} zip/postal code" unless res.code == '200'
 
-      Api::WeatherForecastPresenter.new(res.body).as_json
+      # serialize the required forecast data
+      data = Api::WeatherForecastPresenter.new(res.body).as_json
+
+      # cache data to limit calls to external API
+      cache_results(data)
+
+      data
     end
+  end
+
+  private
+
+  def cache_results(data)
+    Rails.cache.write(zip_code, data.merge(cached: true), expires_in: 30.minutes)
+  end
+
+  def fetch_cached_results(zip_code)
+    Rails.cache.read(zip_code)
+  end
+
+  def api
+    @api ||= WeatherApiService.new
   end
 end
--- spec/services/fetch_weather_forecast_spec.rb
+++ spec/services/fetch_weather_forecast_spec.rb
@@ -2,37 +2,61 @@ require 'rails_helper'
 
 RSpec.describe FetchWeatherForecast do
   let(:zip_code) { [123, '456'].sample }
+  let(:cache) { double }
   let(:api_response) { File.read('spec/fixtures/api_response.json') }
+  let(:forecast_data) do
+    {
+      :name => "Dublin",
+      :region => "California",
+      :country => "USA",
+      :current_weather => {
+        :condition=>"Sunny",
+        :temp_c => 26.1,
+        :temp_f => 79
+      },
+      :forecasted_weather => {
+        :condition => "Partly cloudy",
+        :max_temp_c => 25.8,
+        :max_temp_f => 78.4,
+        :min_temp_c => 7.3,
+        :min_temp_f => 45.1
+      }
+    }
+  end
 
   describe '#call' do
-    context 'when WeatherApiService call is successful' do
-      it 'returns data' do
+    context 'when cached data is found' do
+      it 'returns the cached data' do
+        allow(Rails).to receive(:cache).and_return(cache)
+        expect(cache).to receive(:read).with(zip_code.to_s).and_return(forecast_data)
+
+        expect_any_instance_of(WeatherApiService).not_to receive(:http_get)
+
+        res = described_class.new(zip_code).call
+
+        expect(res).to be_success
+        expect(res.value!).to eq(forecast_data)
+      end
+    end
+
+    context 'when WeatherApiService call succeeds' do
+      it 'caches and returns data' do
+        allow(Rails).to receive(:cache).and_return(cache)
+        expect(cache).to receive(:read).with(zip_code.to_s).and_return(nil)
+
         expect_any_instance_of(WeatherApiService).to receive(:http_get)
           .with({ 'q' => zip_code.to_s })
           .and_return(double(code: '200', body: api_response))
 
         expect_any_instance_of(Api::WeatherForecastPresenter).to receive(:as_json).and_call_original
 
+        expect(cache).to receive(:write)
+          .with(zip_code.to_s, forecast_data.merge(cached: true), expires_in: 30.minutes).once
+
         res = described_class.new(zip_code).call
 
         expect(res).to be_success
-        expect(res.value!).to eq({
-          :name => "Dublin",
-          :region => "California",
-          :country => "USA",
-          :current_weather => {
-            :condition=>"Sunny",
-            :temp_c => 26.1,
-            :temp_f => 79
-          },
-          :forecasted_weather => {
-            :condition => "Partly cloudy",
-            :max_temp_c => 25.8,
-            :max_temp_f => 78.4,
-            :min_temp_c => 7.3,
-            :min_temp_f => 45.1
-          }
-        })
+        expect(res.value!).to eq(forecast_data)
       end
     end
 
